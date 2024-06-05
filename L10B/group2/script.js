ac9c95c2ee1b170229e549467c5a9ab76ac9fc53
gorbetsd <d.gorobets2025@pu.org.ua>
2024-06-05 23:09:12 +0300

    Add files via upload

---
+++ L10B/group2/script.js
@@ -0,0 +1,93 @@
+$(document).ready(function() {
+	$( '.general_information__screen__map' ).show();
+	$( '.general_information__screen__list' ).hide();
+	$( '.general_information__screen__history' ).hide();
+	$( '.general_information__screen__tourist' ).hide();
+	$( '.general_information__screen__food' ).hide();
+	$( '.general_information__screen__source' ).hide();
+	$("#map").click(function(){
+			$( '.general_information__screen__map' ).show();
+			$( '.general_information__screen__list' ).hide();
+			$( '.general_information__screen__history' ).hide();
+			$( '.general_information__screen__tourist' ).hide();
+			$( '.general_information__screen__food' ).hide();
+			$( '.general_information__screen__source' ).hide();
+			$( '#map' ).css('text-decoration', 'underline');
+			$( '#list' ).css('text-decoration', 'none');
+			$( '#history' ).css('text-decoration', 'none');
+			$( '#tourist' ).css('text-decoration', 'none');
+			$( '#food' ).css('text-decoration', 'none');
+			$( '#source' ).css('text-decoration', 'none');
+		});
+	$("#list").click(function(){
+			$( '.general_information__screen__list' ).show();
+			$( '.general_information__screen__map' ).hide();
+			$( '.general_information__screen__history' ).hide();
+			$( '.general_information__screen__tourist' ).hide();
+			$( '.general_information__screen__food' ).hide();
+			$( '.general_information__screen__source' ).hide();
+			$( '#map' ).css('text-decoration', 'none');
+			$( '#list' ).css('text-decoration', 'underline');
+			$( '#history' ).css('text-decoration', 'none');
+			$( '#tourist' ).css('text-decoration', 'none');
+			$( '#food' ).css('text-decoration', 'none');
+			$( '#source' ).css('text-decoration', 'none');
+		});
+	$("#history").click(function(){
+			$( '.general_information__screen__map' ).hide();
+			$( '.general_information__screen__list' ).hide();
+			$( '.general_information__screen__history' ).show();
+			$( '.general_information__screen__tourist' ).hide();
+			$( '.general_information__screen__food' ).hide();
+			$( '.general_information__screen__source' ).hide();
+			$( '#map' ).css('text-decoration', 'none');
+			$( '#list' ).css('text-decoration', 'none');
+			$( '#history' ).css('text-decoration', 'underline');
+			$( '#tourist' ).css('text-decoration', 'none');
+			$( '#food' ).css('text-decoration', 'none');
+			$( '#source' ).css('text-decoration', 'none');
+		});
+	$("#tourist").click(function(){
+			$( '.general_information__screen__map' ).hide();
+			$( '.general_information__screen__list' ).hide();
+			$( '.general_information__screen__history' ).hide();
+			$( '.general_information__screen__tourist' ).show();
+			$( '.general_information__screen__source' ).hide();
+			$( '.general_information__screen__food' ).hide();
+			$( '#map' ).css('text-decoration', 'none');
+			$( '#list' ).css('text-decoration', 'none');
+			$( '#history' ).css('text-decoration', 'none');
+			$( '#tourist' ).css('text-decoration', 'underline');	
+			$( '#food' ).css('text-decoration', 'none');
+			$( '#source' ).css('text-decoration', 'none');
+
+		});
+	$("#food").click(function(){
+			$( '.general_information__screen__map' ).hide();
+			$( '.general_information__screen__list' ).hide();
+			$( '.general_information__screen__history' ).hide();
+			$( '.general_information__screen__tourist' ).hide();
+			$( '.general_information__screen__source' ).hide();
+			$( '.general_information__screen__food' ).show();
+			$( '#map' ).css('text-decoration', 'none');
+			$( '#list' ).css('text-decoration', 'none');
+			$( '#history' ).css('text-decoration', 'none');
+			$( '#tourist' ).css('text-decoration', 'none');
+			$( '#food' ).css('text-decoration', 'underline');
+			$( '#source' ).css('text-decoration', 'none');
+		});
+	$("#source").click(function(){
+			$( '.general_information__screen__map' ).hide();
+			$( '.general_information__screen__list' ).hide();
+			$( '.general_information__screen__history' ).hide();
+			$( '.general_information__screen__tourist' ).hide();
+			$( '.general_information__screen__source' ).show();
+			$( '.general_information__screen__food' ).hide();
+			$( '#map' ).css('text-decoration', 'none');
+			$( '#list' ).css('text-decoration', 'none');
+			$( '#history' ).css('text-decoration', 'none');
+			$( '#tourist' ).css('text-decoration', 'none');
+			$( '#food' ).css('text-decoration', 'none');
+			$( '#source' ).css('text-decoration', 'underline');
+		});
+});
