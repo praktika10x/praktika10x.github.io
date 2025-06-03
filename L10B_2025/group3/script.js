ba5ab6d459f815dabad3d075000fa16781a97230
Liliya-993 <notbit4corpse@gmail.com>
2025-06-03 12:58:50 +0300

    Update script.js

--- L10B_2025/group3/script.js
+++ L10B_2025/group3/script.js
@@ -1,1 +1,16 @@
+const swiper1 = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
 
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
