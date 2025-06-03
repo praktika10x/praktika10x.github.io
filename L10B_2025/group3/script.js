33cb0e4a471ba816b838000408331b593c8d57b4
Liliya-993 <notbit4corpse@gmail.com>
2025-06-03 13:59:58 +0300

    Update script.js

--- L10B_2025/group3/script.js
+++ L10B_2025/group3/script.js
@@ -13,4 +13,8 @@ const swiper1 = new Swiper(".swiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+},
 });
