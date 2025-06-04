b2bd755c69976bedb7831bdd9e5d8bdb35fe0239
bebykhbogdan <b.bebykh2026@pu.org.ua>
2025-06-04 21:27:41 +0300

    script for main page mobile adaptation

--- L10B_2025/group5/lang.js
+++ L10B_2025/group5/lang.js
@@ -8,3 +8,11 @@ function changeURLlanguage(event) {
     const lang = event.target.dataset.lang;
     location.href = window.location.pathname + '#' + lang;
 }
+
+
+const hamburger_icon = document.querySelector('.hamburger-icon');
+hamburger_icon.addEventListener('click', () => {
+    const mobile_nav = document.querySelector('.mobile-menu');
+    mobile_nav.classList.toggle('mobile-menu-active');
+    hamburger_icon.classList.toggle('hamburger-icon-active');
+});
