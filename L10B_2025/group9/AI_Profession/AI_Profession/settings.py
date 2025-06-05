1690112126f690df9af437e1defb517efe9a3a3b
PeterBakun <p.bakun2026@pu.org.ua>
2025-06-05 11:02:36 +0300

    Deploy

--- L10B_2025/group9/AI_Profession/AI_Profession/settings.py
+++ L10B_2025/group9/AI_Profession/AI_Profession/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&imuuo(o$)ad-!_-(2wdeep7$+5yu(vxt%#tolml4&ta12+)1^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['praktika10x-github-io.onrender.com']
 
 
 # Application definition
