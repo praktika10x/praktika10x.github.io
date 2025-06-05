0c50d052bf429bd5341a28aa0961e7a8398053f6
ArtemIvanys <a.ivanys2026@pu.org.ua>
2025-06-05 10:13:03 +0300

    Update lang.js

--- L10B_2025/group5/lang.js
+++ L10B_2025/group5/lang.js
@@ -7,6 +7,7 @@ buttons.forEach(button => {
 function changeURLlanguage(event) {
     const lang = event.target.dataset.lang;
     location.href = window.location.pathname + '#' + lang;
+    location.reload();
 }
 
 
@@ -16,3 +17,27 @@ hamburger_icon.addEventListener('click', () => {
     mobile_nav.classList.toggle('mobile-menu-active');
     hamburger_icon.classList.toggle('hamburger-icon-active');
 });
+
+function changeLanguage() {
+    let lang = select.value;
+    hash = hash.substr(1);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + "en";
+        location.reload();
+    }
+    select.value = hash;
+    document.querySelector('title').innerHTML = langArr['unit'][hash];
+    for (let key in langArr){
+        let el = document.querySelector('.lng-'+key)
+        if(el) {
+        el.innerHTML = langArr[key][hash];}
+    }
+}
+changeLanguage()
+const langArr = {
+    "title": {
+        "ua": "Data-напрям",
+        "en": "Data-field",
+    },
+    
+}
