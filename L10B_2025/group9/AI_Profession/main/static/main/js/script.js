e80bbb719320731d7d5b71aa6112c7a811d64bbe
PeterBakun <p.bakun2026@pu.org.ua>
2025-06-08 13:45:18 +0300

    front update v4

--- L10B_2025/group9/AI_Profession/main/static/main/js/script.js
+++ L10B_2025/group9/AI_Profession/main/static/main/js/script.js
@@ -6,3 +6,6 @@ const header = document.getElementById('header');
 window.addEventListener('scroll', () => {
   header.classList.toggle('shrink', window.scrollY > 50);
 });
+function openTokenizer() {
+  window.open('https://platform.openai.com/tokenizer', '_blank');
+}
